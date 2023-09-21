e1bf9e7830fa5788e014237b06936ba1f16357f3
siradiamrin <145636338+siradiamrin@users.noreply.github.com>
2023-09-21 11:11:30 +0700

    Add files via upload

---
+++ Kota.java
@@ -0,0 +1,20 @@
+package private_basis_data.latihan1;
+
+public class Kota<E> { 
+    private E element;
+    
+    public Kota(E kota){
+        element = kota;
+    }
+
+    public E getElement(){
+        return element;
+    }
+    
+    public static void main(String[] args) {
+        Kota<Integer> jumlahKota = new Kota<Integer>(9);
+        Kota<String> namakota = new Kota<String>("Malang");
+        System.out.println("Jumlah Kota di Jawa Timur : "+ jumlahKota.getElement() + " kota"); 
+        System.out.println("Salah Satu Kota di Jawa Timur : Kota "+namakota.getElement());
+    }
+}
